78e294217b7c91cad71fef0dddbeaf3bc3d2aff7
Yuriy <yurius13@ukr.net>
2024-05-20 08:54:00 +0300

    task-6+

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('#controls button[data-create]');
+const destroyBtn = document.querySelector('#controls button[data-destroy]');
+const inputField = document.querySelector('#controls input');
+const divTags = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", (event) => {
+  let amount = inputField.value;
+  if (amount > 0 && amount <= 100) createBoxes(amount);
+}
+);
+destroyBtn.addEventListener("click", (event) => { destroyBoxes() });
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let markup = "";
+  for (let index = 0; index < amount; index++) {
+    markup += `<div class="div-item"></div>`;
+  }
+  divTags.insertAdjacentHTML("afterbegin", markup);
+}
+
+function destroyBoxes() {
+  divTags.innerHTML = "";
+}
